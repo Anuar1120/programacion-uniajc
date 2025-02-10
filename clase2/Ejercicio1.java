af5bb58bba194ed970fec5fcf48decb527f6fc24
Anuar1120 <aaalegrias@estudiante.uniajc.edu.co>
2025-02-10 21:42:06 +0000

    ejercicio clase2

---
+++ clase2/Ejercicio1.java
@@ -0,0 +1,19 @@
+package clase2;
+
+public class Ejercicio1 {
+     public static void main (String[] args) {
+     //   Area del cuadradro
+     // Declaracion de las variable
+     int lado,area;
+     // asignamos un valor a la variable lado
+      lado= 3;
+     // calculamos el area del cuadrado 
+     area= lado*lado;
+     // Mostramos por pantalla la respuesta 
+     System.out.println ("El area del cuadradado:" + area); 
+
+      }
+ }
+
+
+
