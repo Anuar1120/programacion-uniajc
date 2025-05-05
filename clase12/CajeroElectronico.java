fc94dfe092dc40c42f9d679df3d79a505b82e311
Anuar1120 <aaalegrias@estudiante.uniajc.edu.co>
2025-05-05 17:58:21 +0000

    EjecutarCjeroElectronico

---
+++ clase12/CajeroElectronico.java
@@ -0,0 +1,54 @@
+package clase12;
+
+public class CajeroElectronico {
+    private double saldo;
+
+    public CajeroElectronico(double saldo) {
+        this.saldo = saldo;
+    }
+
+    // Metodos
+    /**
+     * *ontiene lo que tenga asignado el atributo saldo
+     * 
+     * @return
+     */
+
+    public double getSaldo() {
+        return saldo;
+
+    }
+
+    public double depositar(double cantidad) {
+        return saldo += cantidad;
+
+    }
+    public void retirar(double cantidad){
+        if (cantidad <= saldo) {
+            System.out.println("El nuevo saldo es: " + saldo);
+        } else {
+            System.out.println("la cantidad a retirar es mayor al saldo");
+        }
+    }
+
+/**
+ * Obtener el saldo actual del cajero
+ * 
+ *@return saldo
+ */
+
+
+    public double consultarSaldo() {
+        return saldo;
+
+    }
+    public void TRANSFERIR(double cantidad, String destino) {
+        if (cantidad <= saldo) {
+            System.out.println("Se realizo una transferencia: " + cantidad + " a " + destino + " y su saldo nuevo es: " + saldo);
+        } else {
+            System.out.println("La tranferencia no se pudo hacer");
+        }
+    }
+    
+
+}
