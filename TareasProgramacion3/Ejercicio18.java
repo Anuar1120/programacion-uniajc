4e6819ae6b2809aee6d00f9e0e88f3b6d92b04f7
Anuar1120 <aaalegrias@estudiante.uniajc.edu.co>
2026-09-15 03:38:02 +0000

    tarea

--- TareasProgramacion3/Ejercicio18.java
+++ TareasProgramacion3/Ejercicio18.java
@@ -4,7 +4,7 @@ public class Ejercicio18 {
     public static void main(String[] args) {
         int[][] tabla = new int[10][10];
 
-        // Llenar la matriz con las tablas de multiplicar (1 al 10)
+        // Llenar la matriz con las tablas de multiplicar del 1 al 10.
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 tabla[i][j] = (i + 1) * (j + 1);
