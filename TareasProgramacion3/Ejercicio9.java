bc23f2697a3d79a04b89b3adb84dedd7dd126b5f
Anuar1120 <aaalegrias@estudiante.uniajc.edu.co>
2026-09-15 03:04:28 +0000

    tarea9

--- TareasProgramacion3/Ejercicio9.java
+++ TareasProgramacion3/Ejercicio9.java
@@ -1,6 +1,9 @@
 package TareasProgramacion3;
 
 public class Ejercicio9 {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         int[] arreglo = {4, 2, 8, 5, 2, 9, 2, 1, 6};
         int objetivo = 2;
