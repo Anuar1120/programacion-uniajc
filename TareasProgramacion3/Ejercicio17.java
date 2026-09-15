e1fc4def3d1be6e26412cf6c0f2a496943b8ee4b
Anuar1120 <aaalegrias@estudiante.uniajc.edu.co>
2026-09-15 03:31:36 +0000

    tarea

--- TareasProgramacion3/Ejercicio17.java
+++ TareasProgramacion3/Ejercicio17.java
@@ -11,7 +11,7 @@ public class Ejercicio17 {
         int filas = matriz.length;
         int columnas = matriz[0].length;
 
-        // Suma de filas
+        // Suma de las filas
         for (int i = 0; i < filas; i++) {
             int sumaFila = 0;
             for (int j = 0; j < columnas; j++) {
@@ -22,7 +22,7 @@ public class Ejercicio17 {
 
         System.out.println();
 
-        // Suma de columnas
+        // Suma de las columnas
         for (int j = 0; j < columnas; j++) {
             int sumaColumna = 0;
             for (int i = 0; i < filas; i++) {
