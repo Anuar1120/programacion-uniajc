a974ce4c4e5ebdac51f54df5915cb928f21ad921
Anuar1120 <aaalegrias@estudiante.uniajc.edu.co>
2026-09-15 04:10:35 +0000

    tarea

--- TareasProgramacion3/Ejercicio22.java
+++ TareasProgramacion3/Ejercicio22.java
@@ -7,7 +7,7 @@ public class Ejercicio22 {
             {89, 23, 56},
             {7, 34, 90}
         };
-
+        // Tomamos el primer elemento como punto de partida para las comparaciones
         int mayor = matriz[0][0];
         int menor = matriz[0][0];
         int fMayor = 0, cMayor = 0;
@@ -15,11 +15,14 @@ public class Ejercicio22 {
 
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
+                
+                // Actualizamos si encontramos un valor superior al actual
                 if (matriz[i][j] > mayor) {
                     mayor = matriz[i][j];
                     fMayor = i;
                     cMayor = j;
                 }
+                // Actualizamos si encontramos un valor inferior al actual
                 if (matriz[i][j] < menor) {
                     menor = matriz[i][j];
                     fMenor = i;
