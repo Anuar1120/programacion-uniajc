6caaf97875e5a2598b26a24d21a0cfe421f04b6c
Anuar1120 <aaalegrias@estudiante.uniajc.edu.co>
2026-09-15 03:44:34 +0000

    tarea

--- TareasProgramacion3/Ejercicio19.java
+++ TareasProgramacion3/Ejercicio19.java
@@ -2,7 +2,7 @@ package TareasProgramacion3;
 
 public class Ejercicio19 {
     public static void main(String[] args) {
-        int n = 4; // Tamaño de la matriz n x n
+        int n = 4; // El tamaño de la matriz n x n
         int[][] matriz = new int[n][n];
 
         for (int i = 0; i < n; i++) {
